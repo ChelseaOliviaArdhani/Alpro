7cfd915c7e746213538ef84c7127a3964087fb1e
ChelseaOliviaArdhani <celsiseaa@gmail.com>
2025-05-07 15:01:12 +0700

    Add files via upload
    Praktikum 1 Alpro

---
+++ 24221009_celsii/src/pkg24221009_celsii/Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg24221009_celsii;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int nim = 24221009;
+        String nama = "Celsii";     
+        String alamat = "SungaiSelan Kab Bangka Belitung";
+        String TTL = "Sukoharjo, 26 Agustus 2006";
+        
+        System.out.println("biodata");
+        System.out.println("NIM = "+nim);
+        System.out.println("NAMA = "+nama);
+        System.out.println("Alamat = "+alamat);
+        System.out.println("Tempat Tanggal Lahir = "+TTL);
+    
+    }
+    
+    
+}
+ 
